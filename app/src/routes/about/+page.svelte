1e19103cd38abf0630192f8f0ea93e37afb668e2
Eugene Che <indexpro24@gmail.com>
2024-03-18 21:18:03 +0300

    init

---
+++ app/src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	/** @type {import('./$types').PageData} */
+</script>
+
+<svelte:head>
+	<title>"Орбита-строй" | О нашей компании - магазине строительных и отделочных материалов</title>
+	<meta
+		name="description"
+		content="Информация о компании Орбита-строй - интернет-магазине стройматериалов в Дзержинске"
+	/>
+</svelte:head>
+
+<p class="text-red-800">О компании</p>
