7d16ff5a254a485944a1bd042194ec42d247caec
Eugene Che <indexpro24@gmail.com>
2024-03-19 22:53:00 +0300

    c

--- app/src/lib/UI/index.js
+++ app/src/lib/UI/index.js
@@ -1,1 +1,5 @@
 export { default as Test } from './Test/index.svelte';
+
+// Layout
+export { default as Header } from './Header/v1/index.svelte';
+export { default as Footer } from './Footer/v1/index.svelte';
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '../app.css';
+	import { Header, Footer } from '$lib/UI';
 </script>
 
+<Header />
 <slot />
+<Footer />
