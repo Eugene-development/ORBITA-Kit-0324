a7966ae3a6ead2d93c539b4a5fc54b52a1ad8531
Eugene Che <indexpro24@gmail.com>
2024-03-21 13:59:32 +0300

    с

--- app/src/lib/UI/index.js
+++ app/src/lib/UI/index.js
@@ -1,3 +1,3 @@
 // Layout
-export { default as Header } from './Header/v1/index.svelte';
-export { default as Footer } from './Footer/v1/index.svelte';
+export { default as Header } from './Header/index.svelte';
+export { default as Footer } from './Footer/index.svelte';
